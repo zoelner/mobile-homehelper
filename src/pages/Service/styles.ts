23099837c1e665a186754f47f7a4317e93507678
Everton Zoelner <everton.zoelner@gmail.com>
2020-11-23 22:07:28 -0300

    Added Box-shadow

--- src/pages/Service/styles.ts
+++ src/pages/Service/styles.ts
@@ -20,7 +20,9 @@ export const ServiceItem = styled.View`
   border: 1px;
   border-radius: 8px;
   border-color: #8a8a8f;
-  border-width: 0.5px;
+  border-width: 0.25px;
+  background-color: #ffffff;
+  box-shadow: 0px 3px 2px rgba(138, 138, 143, 0.15);
 `;
 
 export const ServiceItemImage = styled.Image`
@@ -28,7 +30,7 @@ export const ServiceItemImage = styled.Image`
   height: 56px;
 
   border-color: #8a8a8f;
-  border-width: 1px;
+  border-width: 0.5px;
   border-radius: 28px;
 `;
 
