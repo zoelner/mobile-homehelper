d1230c74a25bffcd75ab3c25bd41a70303f894f8
Everton Zoelner <everton.zoelner@gmail.com>
2021-06-14 22:49:57 -0300

    Add refreshing budgets

--- src/pages/BudgetsScreens/BudgetList/index.tsx
+++ src/pages/BudgetsScreens/BudgetList/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { FlatList } from 'react-native';
 
 import { ServiceBudgetType } from '~/@types/ServiceBudget';
@@ -11,20 +11,26 @@ import { Container, Header, HeaderText } from './styles';
 
 function BudgetList() {
   const [budgets, setBudgets] = useState([] as ServiceBudgetType[]);
+  const [refreshing, setRefreshing] = useState(false);
 
   const navigation = useNavigation();
 
+  const loadBudgets = useCallback(async () => {
+    const response = await api.get<ServiceBudgetType[]>('service');
+    setBudgets(response.data);
+  }, []);
+
   useEffect(() => {
-    async function loadBudgets() {
-      const response = await api.get<ServiceBudgetType[]>('service');
-
-      setBudgets(response.data);
-    }
-
     const unsubscribe = navigation.addListener('focus', loadBudgets);
 
     return unsubscribe;
-  }, []);
+  }, [loadBudgets]);
+
+  async function refreshingBudgets() {
+    setRefreshing(true);
+    await loadBudgets();
+    setRefreshing(false);
+  }
 
   return (
     <Container>
@@ -38,6 +44,8 @@ function BudgetList() {
         keyExtractor={(budget) => String(budget.id)}
         renderItem={({ item: budget }) => <BudgetItem {...budget} />}
         showsVerticalScrollIndicator={false}
+        onRefresh={refreshingBudgets}
+        refreshing={refreshing}
         ListEmptyComponent={BudgetItemEmpty}
       />
     </Container>
